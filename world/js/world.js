74e95459edfd253a03bda7a2926ec5aa1074b471
Tucian <adrian.tucicovenco@gmail.com>
2025-06-28 23:13:28 +0300

    small fixes

--- world/js/world.js
+++ world/js/world.js
@@ -24,12 +24,15 @@ class World {
 
       this.markings = [];
 
+      this.cars = [];
+      this.bestCar = null;
+
       this.frameCount = 0;
 
       this.generate();
    }
 
-   static load(info) {
+   static load(info){
       const world = new World(new Graph());
       world.graph = Graph.load(info.graph);
       world.roadWidth = info.roadWidth;
@@ -45,7 +48,7 @@ class World {
       world.laneGuides = info.laneGuides.map((g) => new Segment(g.p1, g.p2));
       world.markings = info.markings.map((m) => Marking.load(m));
       world.zoom = info.zoom;
-      world.offset = info.offset
+      world.offset = info.offset;
       return world;
    }
 
@@ -263,20 +266,31 @@ class World {
       this.frameCount++;
    }
 
-   draw(ctx, viewPoint) {
+   draw(ctx, viewPoint, showStartMarkings = true) {
       this.#updateLights();
 
       for (const env of this.envelopes) {
          env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
       }
       for (const marking of this.markings) {
-         marking.draw(ctx);
+         if (!(marking instanceof Start) || showStartMarkings) {
+            marking.draw(ctx);
+         }
       }
       for (const seg of this.graph.segments) {
          seg.draw(ctx, { color: "white", width: 4, dash: [10, 10] });
       }
       for (const seg of this.roadBorders) {
          seg.draw(ctx, { color: "white", width: 4 });
+      }
+
+      ctx.globalAlpha = 0.2;
+      for (const car of this.cars) {
+         car.draw(ctx);
+      }
+      ctx.globalAlpha = 1;
+      if(this.bestCar) {
+         this.bestCar.draw(ctx, true);
       }
 
       const items = [...this.buildings, ...this.trees];
